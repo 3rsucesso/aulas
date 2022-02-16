607a5f05df58b7a5ae8b32f322725919b8de9aab
3rsucesso <99650092+3rsucesso@users.noreply.github.com>
2022-02-16 10:28:49 -0300

    Add files via upload
    Exemplos utilizados na aula dois.

---
+++ Aula_2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Olá, mundo!")
+
+
+# In[2]:
+
+
+print("Estamos na segunda aula do curso.")
+
+
+# In[5]:
+
+
+x = 2
+y = 4
+soma = x + y
+print("A soma de",x ,"mais", y, "é igual a:",soma)
+
+
+# In[9]:
+
+
+x = 24.745684568509
+y = 4
+div = x / y
+print("A divisão de",x ,"mais", y, "é igual a:",div)
+
+
+# In[1]:
+
+
+x = int(input("Escreva o primeiro número: "))
+y = int(input("Escreva o segundo número: "))
+soma = (x+y)
+print("A soma entre {} e {} é igual {}.".format(x,y,soma))
+
+
+# ### 
